23da2e031a125b568aa280699e923dfcf8652c2f
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-08-05 22:25:34 +0500

    work in progress

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-function reload() {
-  alert("please select color first");
-}
-reload();
+// reload();
 let colorSelect = (event) => {
   event.preventDefault();
   let colorPicked = document.querySelector("#colorBox").value;
   console.log(colorPicked);
+
 
   let ctx = document.querySelector("#myCanvas").getContext("2d");
   ctx.fillStyle = `${colorPicked}`;
   // ctx.fillRect(0, 0, 100, 100);
 
   let isMouseDown = false;
-
+  
   document.querySelector("#myCanvas").addEventListener("mousedown", (event) => {
     isMouseDown = true;
+    
   });
   document.querySelector("#myCanvas").addEventListener("mouseup", (event) => {
     isMouseDown = false;
     ctx.beginPath();
+  
   });
 
   document.querySelector("#myCanvas").addEventListener("mousemove", (event) => {
+    
     if (isMouseDown) {
       // console.log("move: ", event);
 
       console.log("h: ", event.offsetX);
       console.log("w: ", event.offsetY);
-
       // ctx.fillRect(event.offsetX, event.offsetY, 2, 2);
 
       ctx.lineWidth = 10;
@@ -38,6 +38,7 @@ let colorSelect = (event) => {
       ctx.moveTo(event.offsetX, event.offsetY)
       ctx.strokeStyle = colorPicked; 
     }
+    
   });
 };
 
